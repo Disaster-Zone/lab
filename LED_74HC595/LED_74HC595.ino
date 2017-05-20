fad89c8e0082c414b47f0d52ad92512926a374e8
Chris Luff <cl_2004@mac.com>
2017-05-20 14:49:26 -0700

    Add 74HC595 to cycle through LEDs.

---
+++ LED_74HC595/LED_74HC595.ino
@@ -0,0 +1,30 @@
+int LATCH_PIN = 5;
+int CLOCK_PIN = 6;
+int DATA_PIN  = 4;
+
+byte leds = 0;
+
+void setup() {
+  pinMode(LATCH_PIN, OUTPUT);
+  pinMode(DATA_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+}
+
+void loop() {
+  leds = 0;
+  updateShiftRegister();
+  delay(500);
+
+  for (int i=0; i < 8; i++) {
+    bitSet(leds, i);
+    updateShiftRegister();
+    delay(500);
+  }
+}
+
+void updateShiftRegister() {
+  digitalWrite(LATCH_PIN, LOW);
+  shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, leds);
+  digitalWrite(LATCH_PIN, HIGH);
+}
+
